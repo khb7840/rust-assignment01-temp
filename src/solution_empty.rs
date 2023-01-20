ff0c421a3319730a144877d73ffd32c71fb85043
Hyunbin Kim <khb7840@gmail.com>
2023-01-20 13:12:34 +0900

    Create solution_empty.rs

---
+++ src/solution_empty.rs
@@ -0,0 +1,20 @@
+// Implement these functions
+
+pub struct NucleotideCounter {
+    pub a: usize,
+    pub c: usize,
+    pub g: usize,
+    pub t: usize,
+}
+
+pub fn counts(dna: &[char]) -> NucleotideCounter {
+    unimplemented!()
+}
+
+pub fn dna_complement(dna: &[char]) -> Vec<char> {
+    unimplemented!()
+}
+
+pub fn reverse_rna_complement(dna: &[char]) -> Vec<char> {
+    unimplemented!()
+}
